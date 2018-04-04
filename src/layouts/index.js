554d69e8a99e34b5e4821b518bed609b83ff3fb4
Sam Wellander <samwellander@gmail.com>
2018-04-04 16:12:14 -0700

    index page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default ({children}) => (
     <div style={{ margin: `00px auto`, maxWidth: 650, padding: `0 1rem` }}>
+        <h3>My Site</h3>
         {children()}
      </div>
 )
